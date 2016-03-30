1189e81129dc053dc6ba9c5f6640503b5289d698
kolamaizik <kolamaizik@gmail.com>
2016-03-30 21:54:41 +0300

    start

---
+++ src/by/training/epam/mk/Stationery.java
@@ -0,0 +1,15 @@
+package by.training.epam.mk;
+
+public abstract class Stationery {
+	private int cost;
+
+	public int getCost() {
+		return cost;
+	}
+
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+	
+	public abstract int countStationery (String... st) ;
+}
